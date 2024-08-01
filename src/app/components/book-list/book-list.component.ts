6f251481f0d35d069bf8d986d73b75fc41dfbff8
Cherhazad <cherhazad.boustani@outlook.fr>
2024-08-01 16:16:47 +0200

    Méthode getBook avec Observable/Observer contrôlée par ng-container

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BookService} from "../../service/book.service";
+import {Observable} from "rxjs";
+import Book from "../../model/Book";
+import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
 
+  books?: Book[];
+
+  constructor(private bookService: BookService) {
+  }
+
+  ngOnInit() {
+    this.bookService.getBooks().subscribe(
+      books => this.books = books
+    );
+
+  }
 }
