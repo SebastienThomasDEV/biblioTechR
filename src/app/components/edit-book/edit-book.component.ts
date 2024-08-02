6a2918f4e26f98d064871be5b84aba95deaf2fc8
Sébastien THOMAS <74055963+SebastienThomasDEV@users.noreply.github.com>
2024-08-02 17:27:25 +0200

    final push

--- src/app/components/edit-book/edit-book.component.ts
+++ src/app/components/edit-book/edit-book.component.ts
@@ -13,12 +13,13 @@ import {ActivatedRoute, Router} from "@angular/router";
     NgClass
   ],
   templateUrl: './edit-book.component.html',
-  styleUrl: './edit-book.component.scss'
 })
 export class EditBookComponent implements OnInit {
 
+  // attribut pour stocker l'id du livre
   bookId: string;
 
+  // Object qui représente le formulaire de création de livre
   formBook: FormGroup = new FormGroup({
     title: new FormControl(''),
     author: new FormControl(''),
@@ -26,10 +27,22 @@ export class EditBookComponent implements OnInit {
     status: new FormControl(''),
   });
 
+  /**
+   * Constructeur du composant
+   * @param bookService
+   * @param activatedRoute
+   * @param router
+   * @return void
+   * */
   constructor(private bookService: BookService, private activatedRoute: ActivatedRoute, private router: Router) {
     this.bookId = this.activatedRoute.snapshot.params['id'];
   }
 
+  /**
+   * Méthode appelée lors de l'initialisation du composant
+   * Elle récupère le livre via l'API
+   * @return void
+   */
   ngOnInit() {
     this.bookService.getBook(this.bookId).subscribe(book => {
       this.formBook.setValue({
@@ -41,6 +54,16 @@ export class EditBookComponent implements OnInit {
     })
   }
 
+  /**
+   * Méthode qui est appelée lors de la soumission du formulaire
+   * Elle récupère les données du formulaire et crée un livre
+   * Elle envoie ensuite le livre créé à l'API
+   *
+   * Si la création est un succès, on notifie l'utilisateur
+   * Sinon, on affiche un error d'erreur
+   * Enfin, on reset le formulaire
+   * @return void
+   */
   onSubmit() {
     if (this.formBook.valid) {
       this.bookService.getBook(this.bookId).subscribe((book)=> {
--- src/app/service/popup.service.ts
+++ src/app/service/popup.service.ts
@@ -12,11 +12,19 @@ export interface Popup {
   providedIn: 'root'
 })
 export class PopupService {
+  // attribut pour stocker des données de type Popup dans un Subject
   private notifySubject = new Subject<Popup>();
 
+  // attribut qui transforme le Subject en Observable pour pouvoir souscrire aux changements dans d'autres composants
   data$ = this.notifySubject.asObservable();
   constructor() { }
 
+  /**
+   * Méthode pour déclencher l'affichage d'un popup
+   * cela declenche une notification à tous les composants qui souscrivent à data$
+   * @param data
+   * @return void
+   * */
   trigger(data: Popup) {
     this.notifySubject.next(data);
   }
